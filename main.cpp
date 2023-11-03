28cfed1a13b3dae81fd8f8126df1231970552a63
AyaUsamaKhalifa <74725538+AyaUsamaKhalifa@users.noreply.github.com>
2023-11-03 10:59:06 +0200

    adds average feature

--- main.cpp
+++ main.cpp
@@ -11,9 +11,21 @@ int getSum(std::vector<int> vec)
     return sum;
 }
 
+float getAverage(std::vector<int> vec)
+{
+    float avg = 0;
+    for(auto element: vec)
+    {
+        avg+=element;
+    }
+    return avg/vec.size();
+}
+
 int main()
 {
     std::vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
     int sum = getSum(vec);
+    float avg = getAverage(vec);
     std::cout << sum << std::endl;
+    std::cout << avg << std::endl;
 }
