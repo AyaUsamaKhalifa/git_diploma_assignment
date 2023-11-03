a5b7eaeb01f87ac78fe91f937fb7e865cd04e276
AyaUsamaKhalifa <74725538+AyaUsamaKhalifa@users.noreply.github.com>
2023-11-03 11:01:21 +0200

    adds min feature

--- main.cpp
+++ main.cpp
@@ -11,9 +11,22 @@ int getSum(std::vector<int> vec)
     return sum;
 }
 
+int getMin(std::vector<int> vec)
+{
+    int min = vec[0];
+    for(auto element: vec)
+    {
+        if(element < min)
+            min = element;
+    }
+    return min;
+}
+
 int main()
 {
     std::vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
     int sum = getSum(vec);
+    int min = getMin(vec);
     std::cout << sum << std::endl;
+    std::cout << min << std::endl;
 }
